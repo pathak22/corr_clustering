13547035f968a3289dc98d402411add1fb513329
WeiCheng Kuo <littlediplomat1002@gmail.com>
2014-11-17 21:54:36 -0800

    SDP Solver Added

---
+++ src/sdp_solver.m
@@ -0,0 +1,64 @@
+str = 'sample.txt';
+
+%Read first line
+[num_nodes, p, q, e] = textread(str, ...
+'%d %f %f %f', 1);
+%p: i=j, q: i!=j, E(i,j)=1, e: i!=j, E(i,j)=0
+
+%Open file
+fileName = str;
+inputfile = fopen(str);
+
+% Skip the first line
+fgetl(inputfile);
+
+% Read in the rest
+tline = fgetl(inputfile);
+i=1;
+C=cell(num_nodes,1);
+while ischar(tline)
+    C{i} = str2num(tline);
+    tline = fgetl(inputfile);
+    i = i+1;
+end
+
+% Read the geometric random graph
+% M = [p,e;e,p];%GRG
+M = zeros(3);%no knowledge about GRG
+
+% Construct p_ij table for G_0
+adj_m = zeros(num_nodes);
+for i = 1:num_nodes
+    conn_i = C{i}(2:end);
+    adj_m(i,i)=0;%pij = 1 for node itself
+    id = C{i}(1);
+    for j = i+1:num_nodes
+        scan_id = C{j}(1);
+        if any(conn_i == j)
+            adj_m(i,j) = 1-M(id,scan_id);%1-p for existing edge in G0
+        else
+            adj_m(i,j) = -1+M(id,scan_id);%p for non-existing edge in G0
+        end
+        
+    end
+end
+        
+w = adj_m+adj_m';
+v = diag(w);
+Dw = diag(v);
+w = w - Dw/2;
+
+% create and solve the problem
+cvx_begin 
+  % A is a PSD symmetric matrix (n-by-n)
+  variable X(num_nodes,num_nodes) semidefinite;
+
+  % constrained matrix entries.
+  diag(X) == ones(num_nodes,1);
+  X >= 0;
+
+  % find the solution to the problem
+  minimize( sum(sum(X.*(1-w)+(1-X).*w)))
+
+cvx_end
+
