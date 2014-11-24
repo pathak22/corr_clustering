4efc3d20c7a976f7f34abaa05f7ab3347783fbc4
Deepak Pathak <deepak.pathak2206@gmail.com>
2014-11-23 18:50:22 -0800

    Model-1 : Random Geom Graph Finalized

--- src/random_geom_model.py
+++ src/random_geom_model.py
@@ -1,5 +1,9 @@
 # ---------------------------------------------------------------------
-# Python script to generate data using geometric random graph
+# Model 1 : Random Geometric Graph
+# Python script to generate data using model-1 and perform greedy pivot
+# algorithm.
+# 
+# Note: All Indices, whatsoever, start from 0.
 # ---------------------------------------------------------------------
 import random
 import networkx as nx
@@ -9,7 +13,7 @@ import numpy as np
 # ---------------------------------------------------------------------
 # Initalize parameters
 # ---------------------------------------------------------------------
-random.seed(222)
+seed = 222
 k = 5			# Number of cluster
 th = 0.3    	# GRG threshold
 ni = 20      	# Number of nodes per cluster
@@ -22,7 +26,9 @@ epsilon = 0.05 	# Inter-cluster disconnected flipping probability
 # ---------------------------------------------------------------------
 # Generate geometric random graph : gr
 # ---------------------------------------------------------------------
-gr = nx.random_geometric_graph(k,th)
+random.seed(seed)
+np.random.seed(seed)
+gr = nx.random_geometric_graph(k,th,seed)
 
 
 # ---------------------------------------------------------------------
@@ -62,13 +68,68 @@ for e in edgeList:
 # ---------------------------------------------------------------------
 # Write the data
 # ---------------------------------------------------------------------
-fid = open('../data/data.txt', 'w')
+fid = open('./data/data_randomGeomModel.txt', 'w')
 fid.write('{} {} {} {}'.format(n,p,q,epsilon))
 for v in gf.nodes_iter():
 	fid.write('\n{} {}'.format(int(v/ni),v))
 	for i in gf.neighbors(v):
 		fid.write(' {}'.format(i))
 fid.close()
+
+
+
+# ---------------------------------------------------------------------
+def pivot_algorithm (gOriginal):
+# ---------------------------------------------------------------------
+# This algorithm is the CC-Pivot (Greedy Algorithm) Algorithm suggested 
+# Ailon et. al. [STOC 2005] and again mentioned in Elsner et. al. [2009]. 
+# For edges with signs +/- and minimization objective, this algorithm 
+# is 3OPT. While for weighted edges, with w_ij^+ + w_ij^- = 1, this 
+# algorithm is 5OPT.
+#
+# This function takes a graph g which is of the form networkx.Graph() 
+# and returns the clustering labels for each node with attribute key as 
+# 'clusterId'.
+#
+# This is tail recursion implementation of algorithm formatted in the 
+# form of loop, till nodes() become empty in temporary graph.
+	clusterId = 100
+	gNew = gOriginal.copy()
+
+	while gNew.nodes():
+		pivot = gNew.nodes()[random.randint(0,len(gNew.nodes())-1)]
+		gOriginal.node[pivot]['clusterId'] = clusterId
+		for v in gNew.neighbors(pivot):
+			gOriginal.node[v]['clusterId'] = clusterId
+		gNew.remove_nodes_from(gNew.neighbors(pivot))
+		gNew.remove_node(pivot)
+		clusterId = clusterId + 1
+
+
+
+
+# ---------------------------------------------------------------------
+# Run Greedy and save output to file
+# ---------------------------------------------------------------------
+pivot_algorithm(gf)
+fid = open('./data/pivot_randomGeomModel.txt', 'w')
+fid.write('# Note:  GroundTruthClusterID ObtainedClusterID dont correspond, they just denote grouping of nodes.\n')
+fid.write('Node GroundTruthClusterID ObtainedClusterID')
+for v in gf.nodes_iter():
+	fid.write('\n{} {} {}'.format(v,int(v/ni),gf.node[v]['clusterId']))
+fid.close()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
